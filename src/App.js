c48ee40c30e170e294cb10024076d9c0ebb2c2b0
santiago montion <santiagomontion@gmail.com>
2024-05-08 21:22:13 -0300

    final

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./component/Home";
 import Navbar from "./component/Navbar";
-
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <Router>
+    <>
       <Navbar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-      </Switch>
-    </Router>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
